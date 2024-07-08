48df82dba1b64a4db76153b0f4e29f313e0d2252
Jefferyagyepong <agyepong9jeff@icloud.com>
2024-07-08 01:01:20 +0000

    init

---
+++ .next/server/chunks/784.js
@@ -0,0 +1,232 @@
+"use strict";
+exports.id = 784;
+exports.ids = [784];
+exports.modules = {
+
+/***/ 3193:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (/* binding */ Footer)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(5384);
+/* harmony import */ var next_link_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link_js__WEBPACK_IMPORTED_MODULE_1__);
+
+
+function Footer() {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("footer", {
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h3", {
+                children: "Quick Links"
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("ul", {
+                children: [
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                        children: [
+                            " ",
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link_js__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                href: "#",
+                                children: " Purchase gift item and coupons"
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                        children: [
+                            " ",
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link_js__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                href: "#",
+                                children: " Order Meal"
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                        children: [
+                            " ",
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link_js__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                href: "#",
+                                children: " Reservations"
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                        children: [
+                            " ",
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link_js__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                href: "#",
+                                children: " Menu today"
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                        children: [
+                            " ",
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link_js__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                href: "#",
+                                children: " Careers"
+                            })
+                        ]
+                    })
+                ]
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h3", {
+                children: "Follow us on social media"
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("ul", {
+                children: [
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                        children: [
+                            " ",
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link_js__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                href: "#",
+                                className: "icon",
+                                target: "_blank"
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                        children: [
+                            " ",
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link_js__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                href: "#",
+                                className: "icon",
+                                target: "_blank"
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                        children: [
+                            " ",
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link_js__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                href: "#",
+                                className: "icon",
+                                target: "_blank"
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                        children: [
+                            " ",
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link_js__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                href: "#",
+                                className: "icon",
+                                target: "_blank"
+                            })
+                        ]
+                    })
+                ]
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h3", {
+                children: "Contact"
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("address", {
+                children: "\uD83D\uDCCD 7 Minetti ST Ak-7174856, Atonsu Kumasi, GH"
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("br", {}),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link_js__WEBPACK_IMPORTED_MODULE_1___default()), {
+                href: "tel:+233244736420",
+                className: "white",
+                children: "\uD83D\uDCDE (233) 244736420"
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("br", {}),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link_js__WEBPACK_IMPORTED_MODULE_1___default()), {
+                href: "mailto:yochorestaurant@gmail.com",
+                className: "white",
+                children: "\uD83D\uDCE7 yochorestaurant@gmail.com"
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("br", {}),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("h6", {
+                children: [
+                    "Yo cho incoporated all rights reserved \xa9 ",
+                    new Date().getFullYear()
+                ]
+            })
+        ]
+    });
+}
+
+
+/***/ }),
+
+/***/ 7673:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (/* binding */ Header)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(1664);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5675);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_image__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+function Header() {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("header", {
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                href: "./",
+                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_image__WEBPACK_IMPORTED_MODULE_2___default()), {
+                    src: "/yocho.svg",
+                    width: 60,
+                    height: 75,
+                    alt: "Jeffery Agyepong logo"
+                })
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("section", {
+                className: "top-nav",
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("input", {
+                        id: "menu-toggle",
+                        type: "checkbox"
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("label", {
+                        className: "menu-button-container",
+                        htmlFor: "menu-toggle",
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                            className: "menu-button"
+                        })
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("ul", {
+                        className: "menu",
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                    href: "/",
+                                    children: "Menu today"
+                                })
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                    href: "/reservation",
+                                    children: "Reservations"
+                                })
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                    href: "/",
+                                    children: "Purchace gift cards and coupons"
+                                })
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                    href: "/",
+                                    children: "Order Meal"
+                                })
+                            })
+                        ]
+                    })
+                ]
+            })
+        ]
+    });
+}
+
+
+/***/ })
+
+};
+;
---
+++ .next/server/pages/index.js
@@ -0,0 +1,394 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 405;
+exports.ids = [405];
+exports.modules = {
+
+/***/ 9345:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ Home)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: external "next/head"
+var head_ = __webpack_require__(968);
+var head_default = /*#__PURE__*/__webpack_require__.n(head_);
+// EXTERNAL MODULE: ./components/Header.js
+var Header = __webpack_require__(7673);
+// EXTERNAL MODULE: ./components/Footer.js
+var Footer = __webpack_require__(3193);
+// EXTERNAL MODULE: ./node_modules/next/link.js
+var next_link = __webpack_require__(1664);
+var link_default = /*#__PURE__*/__webpack_require__.n(next_link);
+// EXTERNAL MODULE: ./node_modules/next/image.js
+var next_image = __webpack_require__(5675);
+var image_default = /*#__PURE__*/__webpack_require__.n(next_image);
+;// CONCATENATED MODULE: ./components/Content1.js
+
+
+
+function Content1() {
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+        className: "content-1",
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx((image_default()), {
+                src: "/yo.png",
+                width: 300,
+                height: 300
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("h5", {
+                children: "Updated May 7th, 2023"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("h2", {
+                children: "NOW ACCEPTING INQUIRIES FOR PRIVATE EVENTS & HOLIDAY PARTIES!"
+            }),
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("p", {
+                children: [
+                    "We are now accepting all inquiries for full buyouts for private events and holiday parties, any day of the week, day or night. Our restaurant can accommodate up to 45 guests and potentially more. We can work with you on the menu, and curate beverages, beer, wine, and sake as well. Please",
+                    " ",
+                    /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                        href: "mailto:jefferyagyepongdev@gmail.com",
+                        children: "Contact us here "
+                    }),
+                    "to inquire!"
+                ]
+            }),
+            " ",
+            /*#__PURE__*/ jsx_runtime_.jsx("h2", {
+                children: "DINNER RESERVATIONS!"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("p", {
+                children: "We are open for dinner service Thursday through Sunday! We'll be accepting limited walk-ins and reservations. For brunch, we still do not take reservationsas we seat guests on first come first serve basis."
+            }),
+            " ",
+            /*#__PURE__*/ jsx_runtime_.jsx("h2", {
+                children: "BUSINESS HOURS"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("p", {
+                children: "BRUNCH: Thursday through Monday 9:00am - 2:30pm"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("p", {
+                children: "DINNER: Thursday through Sunday 5:00pm - 9:00pm"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("p", {
+                children: "CLOSED: Tuesdays & Wednesdays"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("p", {
+                children: "Dine-in, takeout, and delivery available"
+            })
+        ]
+    });
+}
+
+;// CONCATENATED MODULE: ./components/Links.js
+
+
+
+function Links() {
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+        className: "content-1",
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx((link_default()), {
+                href: "#",
+                className: "content-link",
+                children: "ORDER ONLINE"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("br", {}),
+            /*#__PURE__*/ jsx_runtime_.jsx("br", {}),
+            /*#__PURE__*/ jsx_runtime_.jsx((link_default()), {
+                href: "#",
+                className: "content-link",
+                children: "CAREERS - SEND US A RESUME OR SHORT MESSAGE!"
+            })
+        ]
+    });
+}
+
+;// CONCATENATED MODULE: ./components/Cho.js
+
+
+
+function Cho() {
+    return /*#__PURE__*/ jsx_runtime_.jsx("div", {
+        className: "content-1",
+        children: /*#__PURE__*/ jsx_runtime_.jsx("p", {
+            children: "Yo! Cho's kitchen strives to source local, sustainable and organic when possible. We work hard to source premium ingredients and we cook everything from scratch with love. We also do our best to pay our employees living wages and to reduce our environental footprint wherever we can. Overall this factors translate to higher menu prices, but we hope that you find value and feel a sense of comfort in knowing that we can aim to get better everyday at doing what is important to us."
+        })
+    });
+}
+
+;// CONCATENATED MODULE: ./components/Location.js
+
+
+
+function Location() {
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+        className: "content-1",
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx("h2", {
+                children: "L O C A T I O N"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("address", {
+                children: "\uD83D\uDCCD 7 Minetti St Atonsu, AK-717-5846"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("p", {
+                children: "Here are directions to our location. We hope to see you soon!"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("br", {}),
+            /*#__PURE__*/ jsx_runtime_.jsx((link_default()), {
+                href: "#",
+                className: "content-link",
+                children: "Get directions"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("br", {}),
+            /*#__PURE__*/ jsx_runtime_.jsx((link_default()), {
+                href: "#",
+                className: "content-link",
+                children: "Parking Information"
+            })
+        ]
+    });
+}
+
+;// CONCATENATED MODULE: ./pages/index.js
+
+
+
+
+
+
+
+
+
+function Home() {
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+        children: [
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)((head_default()), {
+                children: [
+                    /*#__PURE__*/ jsx_runtime_.jsx("title", {
+                        children: "Yo! Cho"
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
+                        property: "og:title",
+                        content: "Restaurant"
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
+                        property: "og:type",
+                        content: ""
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
+                        property: "og:url",
+                        content: "https://jjefferyagyepong.com"
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
+                        property: "og:image",
+                        content: ""
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
+                        name: "viewport",
+                        content: "width=device-width, initial-scale=1"
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
+                        name: "author",
+                        content: "Jeffery Agyepong"
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
+                        name: "description",
+                        content: "Designer, Front-end Developer, Website development Agency"
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
+                        name: "keywords",
+                        content: "Restaurant, order food online, Ghanaian dishes, beverage "
+                    })
+                ]
+            }),
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("main", {
+                children: [
+                    /*#__PURE__*/ jsx_runtime_.jsx(Header/* default */.Z, {}),
+                    /*#__PURE__*/ jsx_runtime_.jsx(Content1, {}),
+                    /*#__PURE__*/ jsx_runtime_.jsx(Links, {}),
+                    /*#__PURE__*/ jsx_runtime_.jsx(Cho, {}),
+                    /*#__PURE__*/ jsx_runtime_.jsx(Location, {}),
+                    /*#__PURE__*/ jsx_runtime_.jsx(Footer/* default */.Z, {})
+                ]
+            })
+        ]
+    });
+}
+
+
+/***/ }),
+
+/***/ 3918:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/amp-context.js");
+
+/***/ }),
+
+/***/ 5732:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/amp-mode.js");
+
+/***/ }),
+
+/***/ 3280:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/app-router-context.js");
+
+/***/ }),
+
+/***/ 2796:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/head-manager-context.js");
+
+/***/ }),
+
+/***/ 4486:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/image-blur-svg.js");
+
+/***/ }),
+
+/***/ 744:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/image-config-context.js");
+
+/***/ }),
+
+/***/ 5843:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/image-config.js");
+
+/***/ }),
+
+/***/ 9552:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/image-loader");
+
+/***/ }),
+
+/***/ 4964:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router-context.js");
+
+/***/ }),
+
+/***/ 1751:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/add-path-prefix.js");
+
+/***/ }),
+
+/***/ 3938:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/format-url.js");
+
+/***/ }),
+
+/***/ 1109:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/is-local-url.js");
+
+/***/ }),
+
+/***/ 8854:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/parse-path.js");
+
+/***/ }),
+
+/***/ 3297:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/remove-trailing-slash.js");
+
+/***/ }),
+
+/***/ 7782:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/resolve-href.js");
+
+/***/ }),
+
+/***/ 2470:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/side-effect.js");
+
+/***/ }),
+
+/***/ 9232:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/utils.js");
+
+/***/ }),
+
+/***/ 618:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/utils/warn-once.js");
+
+/***/ }),
+
+/***/ 968:
+/***/ ((module) => {
+
+module.exports = require("next/head");
+
+/***/ }),
+
+/***/ 5384:
+/***/ ((module) => {
+
+module.exports = require("next/link.js");
+
+/***/ }),
+
+/***/ 6689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [443,61,784], () => (__webpack_exec__(9345)));
+module.exports = __webpack_exports__;
+
+})();
